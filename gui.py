a0c194172d82a77551c06eabd76c764e56d6f7bf
eshwar3214 <eshwar.smart2001@gmail.com>
2023-05-21 21:31:54 +0530

    changes to gui

--- gui.py
+++ gui.py
@@ -1,55 +1,73 @@
+
+
 import tkinter as tk
 import subprocess
 import signal
 
-processes = []
-
-def start_program_1():
-    process = subprocess.Popen(['python', 'eyecursor.py'])
-    processes.append(process)
-
-def stop_program_1():
-    if processes:
-        process = processes.pop()
-        process.send_signal(signal.SIGTERM)
-
-def start_program_2():
-    process = subprocess.Popen(['python', 'handgesturemouse.py'])
-    processes.append(process)
-
-def stop_program_2():
-    if processes:
-        process = processes.pop()
-        process.send_signal(signal.SIGTERM)
-
-def start_program_3():
-    process = subprocess.Popen(['python', 'subway.py'])
-    processes.append(process)
-
-def stop_program_3():
-    if processes:
-        process = processes.pop()
-        process.send_signal(signal.SIGTERM)
-
 root = tk.Tk()
-root.title("App with Buttons")
+root.title("Gesture controlled user interface")
 
-button1_start = tk.Button(root, text="Start Program 1", command=start_program_1)
-button1_start.pack()
+heading_label = tk.Label(root, text="Gesture-Based Interface Control", font=("Arial", 24, "bold"))
+heading_label.pack(pady=20)
 
-button1_stop = tk.Button(root, text="Stop Program 1", command=stop_program_1)
+process = None
+
+def run_program(program_file):
+    global process
+    process = subprocess.Popen(['python', program_file])
+
+def stop_program():
+    global process
+    if process:
+        process.send_signal(signal.SIGTERM)
+        process = None
+
+# Card 1
+frame1 = tk.Frame(root, padx=10, pady=10)
+frame1.pack(side=tk.LEFT)
+
+image1 = tk.PhotoImage(file="C:\major project\Media\eye.png")
+# Resize the image to a smaller size
+small_image1 = image1.subsample(2, 2)
+label1 = tk.Label(frame1, image=small_image1)
+label1.pack()
+
+button1 = tk.Button(frame1, text="Run eye cursor", command=lambda: run_program("eyecursor.py"),bg="#03fca1")
+button1.pack()
+
+button1_stop = tk.Button(frame1, text="Stop eye cursor", command=stop_program,bg="#fc035e")
 button1_stop.pack()
 
-button2_start = tk.Button(root, text="Start Program 2", command=start_program_2)
-button2_start.pack()
+# Card 2
+frame2 = tk.Frame(root, padx=10, pady=10)
+frame2.pack(side=tk.LEFT)
 
-button2_stop = tk.Button(root, text="Stop Program 2", command=stop_program_2)
+image2 = tk.PhotoImage(file="C:\major project\Media\hand.png")
+# Resize the image to a smaller size
+small_image2 = image2.subsample(2, 2)
+label2 = tk.Label(frame2, image=small_image2)
+label2.pack()
+
+button2 = tk.Button(frame2, text="Run hand gesture cursor", command=lambda: run_program("handgesturemouse.py"),bg="#03fca1")
+button2.pack()
+
+button2_stop = tk.Button(frame2, text="Stop hand gesture cursor", command=stop_program,bg="#fc035e")
 button2_stop.pack()
 
-button3_start = tk.Button(root, text="Start Program 3", command=start_program_3)
-button3_start.pack()
+# Card 3
+frame3 = tk.Frame(root, padx=10, pady=10)
+frame3.pack(side=tk.LEFT)
 
-button3_stop = tk.Button(root, text="Stop Program 3", command=stop_program_3)
+image3 = tk.PhotoImage(file="C:\major project\Media\joy.png")
+# Resize the image to a smaller size
+small_image3 = image3.subsample(2, 2)
+label3 = tk.Label(frame3, image=small_image3)
+label3.pack()
+
+button3 = tk.Button(frame3, text="play game", command=lambda: run_program("subway.py"),bg="#03fca1")
+button3.pack()
+
+button3_stop = tk.Button(frame3, text="Stop game", command=stop_program,bg="#fc035e")
 button3_stop.pack()
 
 root.mainloop()
